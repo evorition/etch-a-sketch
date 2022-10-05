0d89013ba4aa46ab2bcf92cb6d43a85d593f3431
Maxim Makovskiy <makovskiyms@gmail.com>
2022-10-05 18:46:43 +0600

    Add resize button

--- static/script.js
+++ static/script.js
@@ -1,15 +1,50 @@
-const squares = [];
-const grid = document.querySelector(".grid");
+const grid = document.querySelector("#grid");
+const resize = document.querySelector("#resize");
 
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.className = "square";
+resize.addEventListener("click", resizeGrid);
 
-  grid.appendChild(square);
-  squares.push(square);
+function changeColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  this.style.backgroundColor = `#${randomColor}`;
 }
 
-squares.forEach(square => square.addEventListener("mouseover", () => {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  square.style.backgroundColor = `#${randomColor}`;
-}));
+function resizeGrid() {
+  let gridSize;
+
+  do {
+    gridSize = +prompt("Enter a number of squares per side\n Allowed numbers between 1 and 100");
+    console.log(gridSize);
+  } while ((gridSize < 0 || gridSize > 100));
+
+  if (gridSize === 0) {
+    createGrid();
+    return;
+  }
+
+  createGrid(gridSize);
+}
+
+function removeGrid() {
+  while (grid.lastChild) {
+    grid.removeChild(grid.lastChild);
+  }
+}
+
+function createGrid(gridSize = 16) {
+  removeGrid();
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement("div");
+    square.className = "square";
+
+    grid.appendChild(square);
+  }
+
+  grid.style.setProperty("grid-template-columns", `repeat(${gridSize}, 1fr)`);
+  grid.style.setProperty("grid-template-rows", `repeat(${gridSize}, 1fr)`);
+
+  grid.childNodes.forEach(square => square.addEventListener("mouseover",
+    changeColor));
+}
+
+createGrid();
